a7895d6e3c96eea10027543bb9e2f0ec41743dfc
Rafael <12767@holbertonstudents.com>
2026-01-05 00:26:09 +0100

    Task 11: Python function to find schools by topic

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Find schools by topic."""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Find schools that have a specific topic.
+    
+    Args:
+        mongo_collection: pymongo collection object
+        topic: topic to search for
+        
+    Returns:
+        List of school documents that have the topic
+    """
+    return list(mongo_collection.find({"topics": topic}))
