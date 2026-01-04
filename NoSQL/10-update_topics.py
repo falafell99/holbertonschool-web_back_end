835c01c9bdee7c55be849f02769aeade3db620ea
Rafael <12767@holbertonstudents.com>
2026-01-05 00:25:40 +0100

    Task 10: Python function to update school topics

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Update topics of a school document in MongoDB."""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Update topics of a school document.
+    
+    Args:
+        mongo_collection: pymongo collection object
+        name: school name to update
+        topics: list of topics
+    """
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
